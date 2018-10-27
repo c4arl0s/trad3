11d7f4453f2c288a3bd18f3b5d3dbc4d081deb47
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-10-27 18:20:29 -0500

    add a word into title to test if imprimir-titulo.sh is working well, it doesnt

--- imprimir-titulo.sh
+++ imprimir-titulo.sh
@@ -4,7 +4,7 @@ imprimir-titulo()
 echo -e "$BLANCO"
 echo -e ""
 echo -e "$ROJO ======================================================"
-echo -e "$BLANCO BUSCAR PALABRA: (ingresa palabra en ingles/español) "
+echo -e "$BLANCO BUSCAR PALABRA: (ingresa palabra en ingles/español) gHv"
 echo -e "$AMARILLO La ultima palabra buscada fue ... $ROSA $INGLES"
 echo -e "$ROJO ======================================================"
 echo -e "$BLANCO"
