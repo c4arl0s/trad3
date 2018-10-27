894d7166784c47c23b838b957a9b4ce9366afa41
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-10-27 18:30:32 -0500

    I delete the trad3 link into /usr/local/bin

--- imprimir-titulo.sh
+++ imprimir-titulo.sh
@@ -4,7 +4,7 @@ imprimir-titulo()
 echo -e "$BLANCO"
 echo -e ""
 echo -e "$ROJO ======================================================"
-echo -e "$BLANCO BUSCAR PALABRA: (ingresa palabra en ingles/español) gHv"
+echo -e "$BLANCO BUSCAR PALABRA: (ingresa palabra en ingles/español) [DictEnEsScript]"
 echo -e "$AMARILLO La ultima palabra buscada fue ... $ROSA $INGLES"
 echo -e "$ROJO ======================================================"
 echo -e "$BLANCO"
