afb81b10dbbe1a9932d2c470464456ee81a6ca58
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-10-27 19:31:32 -0500

    modify the name of the function to printTitle()

--- trad3.sh
+++ trad3.sh
@@ -13,8 +13,8 @@ while [ "$*" == "" ]
                 #=================================
                 if [[ -z $palabra ]];
                 then
-                                echo "User pressed ENTER with no input text, i will reproduce last word found ...";
-                                reproduce-audio $INGLES
+                    echo "User pressed ENTER with no input text, i will reproduce last word found ...";
+                    reproduce-audio $INGLES
                 else
                     echo -e "$BLANCO existe $ROSA $palabra $BLANCO en base de datos ingles"
                     prueba-palabra-audio $palabra
