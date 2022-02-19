8144e10cf30246850172a38905d73ac366c5a87d
c4arl0s <c.santiago.cruz@gmail.com>
2022-02-19 17:19:26 -0600

    Substitute if statement with case

--- add3.sh
+++ add3.sh
@@ -11,52 +11,51 @@ do
     read ENGLISH
     if [[ $ENGLISH ]]
     then 
-        if [ -z $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt ]
+        if [[ ! -f "$ENGLISH_DIRECTORY_PATH/$ENGLISH.txt" ]]
         then
             echo ""
-            echo "archivo no existe ... se agregara ahora"
+            echo "file does not exist ... read inputs from standard input"
             echo ""
             readInputs
-            echo "confirmacion de la palabra: "
+            echo "this information will be added: "
             echo -e "$WHITE"
             printInputs
-            echo -e "es correcto ? (teclee ENTER) : "
+            echo -e "is correct? (type ENTER key) : "
             echo -e ""
             read CONFIRMATION
-            if [ "$CONFIRMATION" = "" ]
-            then
-                echo -e "$ROSA agregando palabra $WHITE $ENGLISH .... "
-                createEnglishFile
-                saveEnglishTranslation
-                createSpanishFile
-                saveSpanishTranslation        
-                if [ "$PAST" = "" ]
-                then
-                    echo -e " - "    
-                else
-                    touch $ENGLISH_DIRECTORY_PATH/$PAST.txt
-                    echo -e "$PAST es el pasado de $ENGLISH / $SPANISH : " >> $ENGLISH_DIRECTORY_PATH/$PAST.txt
-                fi
-                if [ "$PASTPARTICIPE" = "" ]
-                then      
-                    echo -e " - "    
-                else
-                   touch $ENGLISH_DIRECTORY_PATH/$PASTPARTICIPE.txt
-                   echo -e "$PASTPARTICIPE es el pasado participio de $ENGLISH / $SPANISH : " >> $ENGLISH_DIRECTORY_PATH/$PASTPARTICIPE.txt
-                fi
-                if [ "$GERUND" = "" ]
-                then
-                    echo -e " - "    
-                else
-                    touch $ENGLISH_DIRECTORY_PATH/$GERUND.txt
-                    echo -e "$GERUND es el gerundio de $ENGLISH / $SPANISH : " >> $ENGLISH_DIRECTORY_PATH/$GERUND.txt
-                fi
-                cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
-                cat $SPANISH_DIRECTORY_PATH/$SPANISH.txt
-            else
-                echo ""
-                echo "confirmacion nula : no se agregara la palabra"
-            fi
+            case $CONFIRMATION in
+            "yes") echo -e "$ROSA adding word: $WHITE $ENGLISH .... "
+                   createEnglishFile
+                   saveEnglishTranslation
+                   createSpanishFile
+                   saveSpanishTranslation        
+                   if [ "$PAST" = "" ]
+                   then
+                       echo -e " - "    
+                   else
+                       touch $ENGLISH_DIRECTORY_PATH/$PAST.txt
+                       echo -e "$PAST es el pasado de $ENGLISH / $SPANISH : " >> $ENGLISH_DIRECTORY_PATH/$PAST.txt
+                   fi
+                   if [ "$PASTPARTICIPE" = "" ]
+                   then      
+                       echo -e " - "    
+                   else
+                      touch $ENGLISH_DIRECTORY_PATH/$PASTPARTICIPE.txt
+                      echo -e "$PASTPARTICIPE es el pasado participio de $ENGLISH / $SPANISH : " >> $ENGLISH_DIRECTORY_PATH/$PASTPARTICIPE.txt
+                   fi
+                   if [ "$GERUND" = "" ]
+                   then
+                       echo -e " - "    
+                   else
+                       touch $ENGLISH_DIRECTORY_PATH/$GERUND.txt
+                       echo -e "$GERUND es el gerundio de $ENGLISH / $SPANISH : " >> $ENGLISH_DIRECTORY_PATH/$GERUND.txt
+                   fi
+                   cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
+                   cat $SPANISH_DIRECTORY_PATH/$SPANISH.txt
+                   ;;
+            "no") echo "you said no";;
+            *) echo "Wrong keys";;
+            esac
         else		
             echo -e "$GREEN"
             echo "la palabra existe"
