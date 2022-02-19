575445d75b910f52b4a0e96cee646d1b38befbf1
c4arl0s <c.santiago.cruz@gmail.com>
2022-02-19 14:23:15 -0600

    fix issue about english word was not saved

--- add-functions/printHeader.sh
+++ add-functions/printHeader.sh
@@ -3,7 +3,7 @@
 printHeader() 
 {
     echo ""
-    echo -e "$GREEN DictEnEsScript - SCRIPT to add words english-spanish   $YELLOW  type Ctrl-Z to exit "
+    echo -e "$GREEN DictEnEsScript - SCRIPT to add words english-spanish $YELLOW  type Ctrl-Z to exit "
     echo -e "$RED add only one word in ENGLISH: "
     echo -e "$WHITE " 
 }
