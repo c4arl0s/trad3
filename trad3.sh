feeb2f830baacdfa6b26b3c120f88096308b413b
c4arl0s <c.santiago.cruz@gmail.com>
2022-12-17 23:06:48 -0600

    Add trap to handle control-C

--- trad3.sh
+++ trad3.sh
@@ -7,6 +7,23 @@
 . ./helper-functions/cleanEnglishFile.sh
 . ./helper-functions/cleanSpanishFile.sh
 
+# initialise trap to call trap_ctrlc function
+# when signal 2 (SIGINT) is received
+trap trap_ctrlc 2
+
+# this function is called when Ctrl-C is sent
+function trap_ctrlc ()
+{
+    # perform cleanup here
+    echo "Ctrl-C caught...performing clean up"
+ 
+    echo "Doing cleanup"
+ 
+    # exit shell script with error code 2
+    # if omitted, shell script will continue execution
+    exit 2
+}
+ 
 while [ "$*" = "" ]
 do
     printTitle
