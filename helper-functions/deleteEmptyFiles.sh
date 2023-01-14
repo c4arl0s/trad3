25b7cfa76e00f6cb1369a0f5d87ec69f27d2c1cd
c4arl0s <c.santiago.cruz@gmail.com>
2023-01-14 15:32:46 -0600

    Add severadl words to vocabulary directory

---
+++ helper-functions/deleteEmptyFiles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+deleteEmptyFiles() 
+{
+    find $ENGLISH_DIRECTORY_PATH -type f -empty | while read EMPTY_FILE; do rm $EMPTY_FILE; done
+    find $SPANISH_DIRECTORY_PATH -type f -empty | while read EMPTY_FILE; do rm $EMPTY_FILE; done
+}
