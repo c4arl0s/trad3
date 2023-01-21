d853eb2eaa9c9180a7b7291ad463d655fbb94d40
c4arl0s <c.santiago.cruz@gmail.com>
2023-01-20 22:28:32 -0600

    Fix how spanish words were being saved

--- add-functions/createSpanishFile.sh
+++ add-functions/createSpanishFile.sh
@@ -2,7 +2,9 @@
 
 createSpanishFile() 
 {
+    formattedSPANISH=$(echo $SPANISH | sed "y/āáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜĀÁǍÀĒÉĚÈĪÍǏÌŌÓǑÒŪÚǓÙǕǗǙǛ/aaaaeeeeiiiioooouuuuüüüüAAAAEEEEIIIIOOOOUUUUÜÜÜÜ/")
+    SPANISH=$formattedSPANISH
+    echo "echo $SPANISH"
     touch $SPANISH_DIRECTORY_PATH/$SPANISH.txt
     chmod +x+w $SPANISH_DIRECTORY_PATH/$SPANISH.txt
 }
-
--- add-functions/saveSpanishTranslation.sh
+++ add-functions/saveSpanishTranslation.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 saveSpanishTranslation()
-{
+{ 
+    formattedSPANISH=$(echo $SPANISH | sed "y/āáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜĀÁǍÀĒÉĚÈĪÍǏÌŌÓǑÒŪÚǓÙǕǗǙǛ/aaaaeeeeiiiioooouuuuüüüüAAAAEEEEIIIIOOOOUUUUÜÜÜÜ/")
+    SPANISH=$formattedSPANISH
     echo -e "$SPANISH : $ENGLISH : $SOMETHINGELSE : $PAST : $PASTPARTICIPE : $GERUND : $EXAMPLE" >> $SPANISH_DIRECTORY_PATH/$SPANISH.txt
 }
-
