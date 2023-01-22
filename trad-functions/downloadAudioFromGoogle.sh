333343815c1acadd8289b674c0cc3f4d15906194
c4arl0s <c.santiago.cruz@gmail.com>
2023-01-22 10:25:47 -0600

    Download mp3 only if it does exist

--- trad-functions/downloadAudioFromGoogle.sh
+++ trad-functions/downloadAudioFromGoogle.sh
@@ -5,5 +5,6 @@ downloadAudioFromGoogle()
     WORD=$1
     echo "Downloading $WORD from google"
     URL="https://ssl.gstatic.com/dictionary/static/sounds/de/0"
-    wget -P $AUDIO_DIRECTORY_PATH $URL/$WORD.mp3
+    wget -q --spider $URL/$WORD.mp3 &&
+    wget -c $URL/$WORD.mp3 -O $AUDIO_DIRECTORY_PATH/$WORD.mp3
 }
