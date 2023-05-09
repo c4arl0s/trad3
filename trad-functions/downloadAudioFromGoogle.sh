3564d40a5a177a943ee88b7cc472a88998e04d9d
c4arl0s <c.santiago.cruz@gmail.com>
2023-05-09 09:07:53 -0600

    Improve downloadAudioFromGoogle function in case URL is down

--- trad-functions/downloadAudioFromGoogle.sh
+++ trad-functions/downloadAudioFromGoogle.sh
@@ -5,6 +5,10 @@ downloadAudioFromGoogle()
     WORD=$1
     echo "Downloading $WORD from google"
     URL="https://ssl.gstatic.com/dictionary/static/sounds/de/0"
-    wget -q --spider $URL/$WORD.mp3 &&
-    wget -c $URL/$WORD.mp3 -O $AUDIO_DIRECTORY_PATH/$WORD.mp3
+    if wget -q --spider $URL/$WORD.mp3 && wget -c $URL/$WORD.mp3 -O $AUDIO_DIRECTORY_PATH/$WORD.mp3 
+    then
+        echo "downloading mp3 file"
+    else 
+        echo "URL is down"
+    fi
 }
