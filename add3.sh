81dda73fc857c08d3143103a4e641250271b7915
c4arl0s <c.santiago.cruz@gmail.com>
2023-05-10 14:10:15 -0600

    Update first if statement and remove ! character in order to fix logic

--- add3.sh
+++ add3.sh
@@ -7,77 +7,76 @@ while [ "$*" = "" ]
 do
     printHeader
     read ENGLISH
-    if [[ $ENGLISH ]]
+    if $(isRetrievableEnglishWord $ENGLISH)
+    then
+        echo -e "$GREEN"
+        echo "$ENGLISH exist"
+        echo -e "$WHITE"
+        cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
+        echo ""
+        echo -e "$RED"
+        echo "¿ Do you wish to add another meaning? type: [yes/no/edit]? "
+        echo -e " $WHITE"
+        read OPTION
+        case $OPTION in
+        "yes") echo -e "$RED"
+               readInputs
+               echo "these are your changes: "
+               printInputs
+               echo "Type yes to confirm"
+               read OPTION
+               case $OPTION in 
+                   "yes") echo -e "$RED"
+                          echo -e "Saving $ENGLISH ..."
+                          saveEnglishTranslation
+                          cleanEnglishFile $ENGLISH
+                          saveSpanishTranslation 
+                          saveVerb $PAST $PASTPARTICIPE $GERUND
+                          cleanSpanishFile $SPANISH
+                          cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt;;
+                    "no") echo -e "$RED"
+                          echo "You cancelled operation";;
+                    *) echo "Type correct key";;
+              esac;;
+        "no") echo ""
+    	  echo "Create a task when you say no";;
+        "edit") echo ""
+                vim $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
+                vim $SPANISH_DIRECTORY_PATH/$SPANISH.txt;;
+        *) echo "type correct key"
+           echo "";;
+        esac
+    elif [ "$ENGLISH" = "" ]
     then 
-        if ! $(isRetrievableEnglishWord $ENGLISH)
-        then
-            echo -e "$GREEN"
-            echo "$ENGLISH exist"
-            echo -e "$WHITE"
-            cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
-            echo ""
-            echo -e "$RED"
-            echo "¿ Do you wish to add another meaning? type: [yes/no/edit]? "
-            echo -e " $WHITE"
-            read OPTION
-            case $OPTION in
-            "yes") echo -e "$RED"
-                   readInputs
-                   echo "these are your changes: "
-                   printInputs
-                   echo "Type yes to confirm"
-                   read OPTION
-                   case $OPTION in 
-                       "yes") echo -e "$RED"
-                              echo -e "Saving $ENGLISH ..."
-                              saveEnglishTranslation
-                              cleanEnglishFile $ENGLISH
-                              saveSpanishTranslation 
-                              saveVerb $PAST $PASTPARTICIPE $GERUND
-                              cleanSpanishFile $SPANISH
-                              cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt;;
-                        "no") echo -e "$RED"
-                              echo "You cancelled operation";;
-                        *) echo "Type correct key";;
-                  esac;;
-            "no") echo ""
-        	  echo "Create a task when you say no";;
-            "edit") echo ""
-                    vim $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
-                    vim $SPANISH_DIRECTORY_PATH/$SPANISH.txt;;
-            *) echo "type correct key"
-               echo "";;
-            esac
-        else
-            echo ""
-            echo "file does not exist ... read inputs from standard input"
-            echo ""
-            readInputs
-            echo "this information will be added: "
-            echo -e "$WHITE"
-            printInputs
-            echo -e ""
-            echo -e "Select a number: "
-            echo -e ""
-            select CONFIRMATION in yes no quit; do
-            case $CONFIRMATION in
-            yes) echo -e "$ROSA adding word: $WHITE $ENGLISH .... "
-                   createEnglishFile
-                   saveEnglishTranslation
-                   createSpanishFile
-                   saveSpanishTranslation        
-                   saveVerb $PAST $PASTPARTICIPE $GERUND
-                   cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
-                   cat $SPANISH_DIRECTORY_PATH/$SPANISH.txt
-                   break;;
-            no) echo "you said no"
-                break;;
-            quit) break;;
-            *) echo "Wrong keys";;
-            esac
-            done
-        fi
+        echo "$CYAN"
+        echo "Empty word"
     else
-        echo "Type a word"
-    fi	
+        echo ""
+        echo "file does not exist ... read inputs from standard input"
+        echo ""
+        readInputs
+        echo "this information will be added: "
+        echo -e "$WHITE"
+        printInputs
+        echo -e ""
+        echo -e "Select a number: "
+        echo -e ""
+        select CONFIRMATION in yes no quit; do
+        case $CONFIRMATION in
+        yes) echo -e "$ROSA adding word: $WHITE $ENGLISH .... "
+               createEnglishFile
+               saveEnglishTranslation
+               createSpanishFile
+               saveSpanishTranslation        
+               saveVerb $PAST $PASTPARTICIPE $GERUND
+               cat $ENGLISH_DIRECTORY_PATH/$ENGLISH.txt
+               cat $SPANISH_DIRECTORY_PATH/$SPANISH.txt
+               break;;
+        no) echo "you said no"
+            break;;
+        quit) break;;
+        *) echo "Wrong keys";;
+        esac
+        done
+    fi
 done
