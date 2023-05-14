1e1b54776a35f94aad55b5f21f5ee37470b28379
c4arl0s <c.santiago.cruz@gmail.com>
2023-05-13 22:06:56 -0600

    Remove a directory repetition echo command

--- trad-functions/cleanSpanishFile.sh
+++ trad-functions/cleanSpanishFile.sh
@@ -5,7 +5,6 @@ cleanSpanishFile()
     SPANISH_FILE=$1
     # remove empty lines
     echo "directory $SPANISH_DIRECTORY_PATH"
-    echo "directory $COMODIN_DIRECTORY_PATH"
     sed "/^ *$/d" $SPANISH_DIRECTORY_PATH/$SPANISH_FILE.txt > $COMODIN_DIRECTORY_PATH/temporal.txt
     cat $COMODIN_DIRECTORY_PATH/temporal.txt > $SPANISH_DIRECTORY_PATH/$SPANISH_FILE.txt
 }
