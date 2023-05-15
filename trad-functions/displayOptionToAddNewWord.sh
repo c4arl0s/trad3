55ebab39c75c41e345b6bad6422bca6e211eebca
c4arl0s <c.santiago.cruz@gmail.com>
2023-05-15 13:53:22 -0600

    Update displayOptionToAddNewWord.sh according to new color

--- trad-functions/displayOptionToAddNewWord.sh
+++ trad-functions/displayOptionToAddNewWord.sh
@@ -4,6 +4,6 @@ displayOptionToAddNewWord()
 {
     printf "\n${WHITE}$espanol : $ingles"
     printf "\n${CYAN}Do you want to add this word?:${PINK} $WORD?"
-    printf "\n${YELLOW}Type your choice [${GREEN}yes${YELLOW}|${GREEN}add3${YELLOW}|${GREEN}↩${YELLOW}], type enter to ignore"
-    echo -e "${YELLOW}"
+    printf "\n${YELLOW}Type your choice [${GREEN}yes${YELLOW}|${GREEN}add3${YELLOW}|${GREEN}↩${YELLOW}], type enter to ignore:"
+    echo -e "${GREEN}"
 }
