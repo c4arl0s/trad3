8e9cf0e43afb555469541797ab4a270b54ab3cf6
c4arl0s <c.santiago.cruz@gmail.com>
2023-05-14 19:15:00 -0600

    Remove an additional englush audio reproduction

--- trad-functions/displayOptionToAddNewWord.sh
+++ trad-functions/displayOptionToAddNewWord.sh
@@ -2,9 +2,8 @@
 
 displayOptionToAddNewWord()
 {
-    echo -e "${WHITE}$espanol : $ingles"
-    echo -e "${CYAN}Do you want to add this word?${PINK} $WORD?"
-    reproduceEnglishAudioFileIfAvailable $ingles
-    echo -e "${YELLOW}Type your choice [${GREEN}yes${YELLOW}|${GREEN}add3${YELLOW}|${GREEN}↩${YELLOW}], type enter to ignore"
+    printf "\n${WHITE}$espanol : $ingles"
+    printf "\n${CYAN}Do you want to add this word?:${PINK} $WORD?"
+    printf "\n${YELLOW}Type your choice [${GREEN}yes${YELLOW}|${GREEN}add3${YELLOW}|${GREEN}↩${YELLOW}], type enter to ignore"
     echo -e "${YELLOW}"
 }
--- trad-functions/downloadAudioFromGoogle.sh
+++ trad-functions/downloadAudioFromGoogle.sh
@@ -7,8 +7,8 @@ downloadAudioFromGoogle()
     URL="https://ssl.gstatic.com/dictionary/static/sounds/de/0"
     if wget -q --spider $URL/$WORD.mp3 && wget -c $URL/$WORD.mp3 -O $AUDIO_DIRECTORY_PATH/$WORD.mp3 
     then
-        echo "${GREEN}downloading mp3 file"
+        echo "${GREEN}downloading mp3 file ..."
     else 
-        echo "${RED}URL is down"
+        echo "${RED}URL is down, try other mean"
     fi
 }
