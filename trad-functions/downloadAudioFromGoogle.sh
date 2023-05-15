96dbeb970595a4453aefdcb8f79d3c8815d2f307
c4arl0s <c.santiago.cruz@gmail.com>
2023-05-14 20:18:34 -0600

    Improve downloadAudioFromGoogle function

--- trad-functions/downloadAudioFromGoogle.sh
+++ trad-functions/downloadAudioFromGoogle.sh
@@ -3,12 +3,12 @@
 downloadAudioFromGoogle()
 {
     WORD=$1
-    echo "${WHITE}Downloading $WORD from google"
+    printf "\n${YELLOW}Trying Downloading from google"
     URL="https://ssl.gstatic.com/dictionary/static/sounds/de/0"
     if wget -q --spider $URL/$WORD.mp3 && wget -c $URL/$WORD.mp3 -O $AUDIO_DIRECTORY_PATH/$WORD.mp3 
     then
-        echo "${GREEN}downloading mp3 file ..."
+        printf "\n${GREEN}downloading mp3 file ..."
     else 
-        echo "${RED}URL is down, try other mean"
+        printf "\n${RED}URL is down, try other mean"
     fi
 }
