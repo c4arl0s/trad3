9489b98191fdeb83d2bd0af3723d3de9a0d10928
c4arl0s <c.santiago.cruz@gmail.com>
2023-05-18 05:56:41 -0600

    Add message when is trying to find a word by using google script

--- trad3.sh
+++ trad3.sh
@@ -31,6 +31,7 @@ do
             reproduceEnglishAudioFileIfAvailable $cleanEnglishWord
             LAST_WORD_FOUND=$cleanEnglishWord
         else
+            printf("\n${}It seems that we can find it using google script")
             searchWordUsingGoogleScript $WORD
             reproduceEnglishAudioFileIfAvailable $ingles 
             displayOptionToAddNewWord
