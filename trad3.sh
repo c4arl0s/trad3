ca6d3262b333e6dae005967c87d18cc615bc2b54
c4arl0s <c.santiago.cruz@gmail.com>
2024-02-05 14:38:42 -0600

    Apply Google Bash style

--- trad3.sh
+++ trad3.sh
@@ -14,31 +14,31 @@ do
     printTitle $(basename $0)
     echo -e "${WHITE}"
     printf "%s" "Type a word: "; read WORD
-    if $(isRetrievableEnglishWord $WORD)
+    if $(isRetrievableEnglishWord ${WORD})
     then
-        printf "\n${WHITE}$WORD ${GREEN}is available in english data base"
-        reproduceEnglishAudioFileIfAvailable $WORD
-        displayEnglishTranslation $WORD
-        LAST_WORD_FOUND=$WORD
-    elif [ -z "$WORD" ]
+        printf "\n${WHITE}${WORD} ${GREEN}is available in english data base"
+        reproduceEnglishAudioFileIfAvailable ${WORD}
+        displayEnglishTranslation ${WORD}
+        LAST_WORD_FOUND=${WORD}
+    elif [ -z "${WORD}" ]
     then
         echo -e "${RED}Empty word"        
-        reproduceLastWordFoundIfAvailable $LAST_WORD_FOUND
+        reproduceLastWordFoundIfAvailable ${LAST_WORD_FOUND}
     else
-        printf "\n${WHITE}$WORD ${RED}does not exist on english data base .... now looking into spanish data base"
-        if $(isRetrievableSpanishWord $WORD)
+        printf "\n${WHITE}${WORD} ${RED}does not exist on english data base .... now looking into spanish data base"
+        if $(isRetrievableSpanishWord ${WORD})
         then
-            printf "\n${WHITE}$WORD ${RED}does exist on spanish data base"
-            SPANISH_WORD=$WORD
-            cleanSpanishFile $SPANISH_WORD
-            displaySpanishTranslation $SPANISH_WORD
+            printf "\n${WHITE}${WORD} ${RED}does exist on spanish data base"
+            SPANISH_WORD=${WORD}
+            cleanSpanishFile ${SPANISH_WORD}
+            displaySpanishTranslation ${SPANISH_WORD}
             cleanEnglishWord=$(echo "${INGLES}" | xargs)
-            reproduceEnglishAudioFileIfAvailable $cleanEnglishWord
-            LAST_WORD_FOUND=$cleanEnglishWord
+            reproduceEnglishAudioFileIfAvailable ${cleanEnglishWord}
+            LAST_WORD_FOUND=${cleanEnglishWord}
         else
             printf "It seems that we can find ${WORD} using google script"
-            searchWordUsingGoogleScript $WORD
-            reproduceEnglishAudioFileIfAvailable $ingles 
+            searchWordUsingGoogleScript ${WORD}
+            reproduceEnglishAudioFileIfAvailable ${ingles} 
             displayOptionToAddNewWord
             displayMenu
         fi
