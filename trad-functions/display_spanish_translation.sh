b91c6f4c54ae699230dc41665a2d8b9d6e397a82
c4arl0s <c.santiago.cruz@gmail.com>
2024-10-27 16:31:09 -0600

    Fix printable variables

--- trad-functions/display_spanish_translation.sh
+++ trad-functions/display_spanish_translation.sh
@@ -4,7 +4,7 @@ display_spanish_translation()
 {
     spanish_word=$1
     rows=$(count_lines_spanish_file ${spanish_word})
-    printf "\n${spanish_word} file has $rows rows\n"
+    # printf "\n${spanish_word} file has $rows rows\n"
     for row in `seq ${rows}`
     do
         get_spanish_fields ${spanish_word} ${row}
