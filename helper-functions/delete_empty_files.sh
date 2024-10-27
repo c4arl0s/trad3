7ae219b9471ab8634bdc2af534d0844ab65f407c
c4arl0s <c.santiago.cruz@gmail.com>
2024-10-27 12:03:04 -0600

    Update method name to delete_empty_files

--- helper-functions/delete_empty_files.sh
+++ helper-functions/delete_empty_files.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-deleteEmptyFiles() 
+delete_empty_files()
 {
    find ${ENGLISH_DIRECTORY_PATH} -type f -empty | while read EMPTY_FILE; do rm ${EMPTY_FILE}; done
    find ${SPANISH_DIRECTORY_PATH} -type f -empty | while read EMPTY_FILE; do rm ${EMPTY_FILE}; done
