54315e5d203b5b341c6c3c2a266066fc4c261b84
c4arl0s <c.santiago.cruz@gmail.com>
2025-03-30 07:49:16 -0600

    Fix issue when you were trying to translate a spanish word

--- trad-functions/print_spanish_fields.sh
+++ trad-functions/print_spanish_fields.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-print_spanish_fields()
-{
-    echo -e "${WHITE}${INGLES}:${PINK} ${SPANISH_WORD}"
-    echo ""
-    echo -e "${WHITE}Extra:${GREEN} ${EXTRA}"
-    echo -e "${WHITE}Past:${GREEN} ${P}"
-    echo -e "${WHITE}Past Participe:${GREEN} ${PP}"
-    echo -e "${WHITE}Gerund:${GREEN} ${G}"
-    echo -e "${WHITE}Example:${GREEN} ${E}"
+print_spanish_fields() {
+  echo -e "${INGLES}:${ESPANOL}"
+  echo ""
+  echo -e "${WHITE}Extra:${GREEN} ${EXTRA}"
+  echo -e "${WHITE}Past:${GREEN} ${P}"
+  echo -e "${WHITE}Past Participe:${GREEN} ${PP}"
+  echo -e "${WHITE}Gerund:${GREEN} ${G}"
+  echo -e "${WHITE}Example:${GREEN} ${E}"
 }
